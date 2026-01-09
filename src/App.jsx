ca13eea8073a3e2fb5077fb0abc717f1e8444279
serranolash <serranolash@gmail.com>
2026-01-09 12:57:06 -0300

    Agregar Admin a la Landing version mejorada sin errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import LandingPage from "./LandingPage";
+import { Toaster } from 'react-hot-toast';
 
 export default function App() {
-  return <LandingPage />;
+  return (
+    <>
+      <LandingPage />
+      <Toaster position="top-right" />
+    </>
+  );
 }
